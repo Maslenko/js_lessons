01fa4f62ffb4238d3dafccadfefc87c2ec3e0b69
Maslenko <alexmaslenko@gmail.com>
2025-09-14 21:39:03 +0200

    js_lessons

---
+++ js/main.js
@@ -0,0 +1,9 @@
+		//Комментарий 
+		/* Многострочный 
+		комментарий */
+		document.write("Привет от Мазилы Бабашкина - JavaScript");
+		document.write("Ещё раз привет от JavaScript");
+		console.log("Привет от JavaScrip для console"); 
+		console.info("То же что и log");
+		console.error("Привет от JS. Ошибка!");
+		console.warn("Уведомление от JS.");
